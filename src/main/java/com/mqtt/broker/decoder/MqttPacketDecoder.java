0eaccfc484c1b1061da3be14efd00c0968dac2f1
Karol Bystrek <karbystrek@gmail.com>
2025-12-26 15:20:45 +0100

    Refactor packet decoder package 2

--- src/main/java/com/mqtt/broker/decoder/MqttPacketDecoder.java
+++ src/main/java/com/mqtt/broker/decoder/MqttPacketDecoder.java
@@ -1,44 +1,24 @@
 package com.mqtt.broker.decoder;
 
 import com.mqtt.broker.packet.MqttPacket;
-import com.mqtt.broker.packet.MqttPacketType;
 
 import java.nio.ByteBuffer;
-import java.util.Map;
 
 import static com.mqtt.broker.decoder.DecoderUtils.decodeFixedHeader;
 import static com.mqtt.broker.exception.UnsupportedPacketTypeException.unsupportedPacketType;
-import static com.mqtt.broker.packet.MqttPacketType.CONNECT;
-import static com.mqtt.broker.packet.MqttPacketType.DISCONNECT;
-import static com.mqtt.broker.packet.MqttPacketType.PINGREQ;
-import static com.mqtt.broker.packet.MqttPacketType.PUBACK;
-import static com.mqtt.broker.packet.MqttPacketType.PUBCOMP;
-import static com.mqtt.broker.packet.MqttPacketType.PUBLISH;
-import static com.mqtt.broker.packet.MqttPacketType.PUBREC;
-import static com.mqtt.broker.packet.MqttPacketType.PUBREL;
-import static com.mqtt.broker.packet.MqttPacketType.SUBSCRIBE;
-import static com.mqtt.broker.packet.MqttPacketType.UNSUBSCRIBE;
-import static java.util.Map.entry;
-import static java.util.Map.ofEntries;
 
 public class MqttPacketDecoder {
 
-    private final Map<MqttPacketType, PacketDecoder<?>> decoders;
-
-    public MqttPacketDecoder() {
-        this.decoders = ofEntries(
-                entry(CONNECT, new ConnectPacketDecoder()),
-                entry(DISCONNECT, new DisconnectPacketDecoder()),
-                entry(PUBLISH, new PublishPacketDecoder()),
-                entry(PUBACK, new PubAckPacketDecoder()),
-                entry(PINGREQ, new PingReqPacketDecoder()),
-                entry(PUBREC, new PubRecPacketDecoder()),
-                entry(PUBREL, new PubRelPacketDecoder()),
-                entry(PUBCOMP, new PubCompPacketDecoder()),
-                entry(SUBSCRIBE, new SubscribePacketDecoder()),
-                entry(UNSUBSCRIBE, new UnsubscribePacketDecoder())
-        );
-    }
+    private final ConnectPacketDecoder connect = new ConnectPacketDecoder();
+    private final DisconnectPacketDecoder disconnect = new DisconnectPacketDecoder();
+    private final PublishPacketDecoder publish = new PublishPacketDecoder();
+    private final PubAckPacketDecoder pubAck = new PubAckPacketDecoder();
+    private final PingReqPacketDecoder pingReq = new PingReqPacketDecoder();
+    private final PubRecPacketDecoder pubRec = new PubRecPacketDecoder();
+    private final PubRelPacketDecoder pubRel = new PubRelPacketDecoder();
+    private final PubCompPacketDecoder pubComp = new PubCompPacketDecoder();
+    private final SubscribePacketDecoder subscribe = new SubscribePacketDecoder();
+    private final UnsubscribePacketDecoder unsubscribe = new UnsubscribePacketDecoder();
 
     public MqttPacket decode(ByteBuffer buffer) {
         if (buffer.remaining() < 2) {
@@ -58,14 +38,18 @@ public class MqttPacketDecoder {
 
         buffer.position(buffer.position() + fixedHeader.remainingLength());
 
-        return getDecoderFor(fixedHeader.packetType()).decode(fixedHeader, packetBody);
-    }
-
-    private PacketDecoder<?> getDecoderFor(MqttPacketType packetType) {
-        var decoder = decoders.get(packetType);
-        if (decoder == null) {
-            throw unsupportedPacketType(packetType);
-        }
-        return decoder;
+        return switch (fixedHeader.packetType()) {
+            case CONNECT -> connect.decode(fixedHeader, packetBody);
+            case DISCONNECT -> disconnect.decode(fixedHeader, packetBody);
+            case PUBLISH -> publish.decode(fixedHeader, packetBody);
+            case PUBACK -> pubAck.decode(fixedHeader, packetBody);
+            case PINGREQ -> pingReq.decode(fixedHeader, packetBody);
+            case PUBREC -> pubRec.decode(fixedHeader, packetBody);
+            case PUBREL -> pubRel.decode(fixedHeader, packetBody);
+            case PUBCOMP -> pubComp.decode(fixedHeader, packetBody);
+            case SUBSCRIBE -> subscribe.decode(fixedHeader, packetBody);
+            case UNSUBSCRIBE -> unsubscribe.decode(fixedHeader, packetBody);
+            default -> throw unsupportedPacketType(fixedHeader.packetType());
+        };
     }
 }
